52b4d596522fdabc9e961feb8dcb065fb350f7c3
German Fica <germafica@gmail.com>
2022-01-14 02:41:54 -0300

    Delete boilerplate code from User

--- src/main/java/com/bezkoder/springjwt/models/User.java
+++ src/main/java/com/bezkoder/springjwt/models/User.java
@@ -1,5 +1,9 @@
 package com.bezkoder.springjwt.models;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,6 +12,9 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+@Getter
+@Setter
+@NoArgsConstructor // JPA requires that this constructor be defined as public or protected
 @Entity
 @Table(name = "users", 
     uniqueConstraints = { 
@@ -15,6 +22,7 @@ import javax.validation.constraints.Size;
       @UniqueConstraint(columnNames = "email") 
     })
 public class User {
+  // == fields ==
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
@@ -32,58 +40,23 @@ public class User {
   @Size(max = 120)
   private String password;
 
+  // == relationships ==
   @ManyToMany(fetch = FetchType.LAZY)
   @JoinTable(  name = "user_roles", 
         joinColumns = @JoinColumn(name = "user_id"), 
         inverseJoinColumns = @JoinColumn(name = "role_id"))
   private Set<Role> roles = new HashSet<>();
 
-  public User() {
-  }
-
+  // == constructors ==
+  /**
+   * Constructs an {@code User} object initialized with the given values.
+   * @param username the username of the user
+   * @param email the email of the user
+   * @param password the password of the user
+   */
   public User(String username, String email, String password) {
     this.username = username;
     this.email = email;
     this.password = password;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getEmail() {
-    return email;
-  }
-
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  public Set<Role> getRoles() {
-    return roles;
-  }
-
-  public void setRoles(Set<Role> roles) {
-    this.roles = roles;
   }
 }
